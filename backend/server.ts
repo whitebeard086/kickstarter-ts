c2ab1469fd8d53ea4e51092fb91897ccf463b4eb
The Exorcist <whitebeard086@gmail.com>
2022-09-10 19:33:54 +0100

    Express setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import * as express from "express";
+import { PORT } from "./utils/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
