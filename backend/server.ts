0d8db19603896b32c064d4e9613e5319f85b3e79
The Exorcist <whitebeard086@gmail.com>
2022-09-11 19:08:41 +0100

    Projects routes

--- backend/server.ts
+++ backend/server.ts
@@ -6,4 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/projects", require("./routes/projectRoutes"));
+
 app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
