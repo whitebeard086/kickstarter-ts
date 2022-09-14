1096282bf4637d8483cb85a304c6af0810c2fd48
The Exorcist <whitebeard086@gmail.com>
2022-09-14 13:58:11 +0100

    project model

--- backend/controllers/projectsController.ts
+++ backend/controllers/projectsController.ts
@@ -1,39 +1,41 @@
 import { Response, Request } from "express";
+const asyncHandler = require("express-async-handler");
 
 // @desc Get all projects
 // @route GET /api/projects
 // @access Public
-const getProjects = (req: Request, res: Response) => {
+const getProjects = asyncHandler(async (req: Request, res: Response) => {
+  const projects = await Project.find();
   res.status(200).json({ message: "Get All Projects" });
-};
+});
 
 // @desc Create a project
 // @route POST /api/projects
 // @access Private
-const createProject = (req: Request, res: Response) => {
+const createProject = asyncHandler(async (req: Request, res: Response) => {
   res.status(201).json({ message: "Create a Project" });
-};
+});
 
 // @desc Get a project
 // @route GET /api/projects/:id
 // @access Public
-const getProject = (req: Request, res: Response) => {
+const getProject = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Get Project ${req.params.id}` });
-};
+});
 
 // @desc Update a project
 // @route PUT /api/projects/:id
 // @access Private
-const updateProject = (req: Request, res: Response) => {
+const updateProject = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Update Project ${req.params.id}` });
-};
+});
 
 // @desc Delete a project
 // @route DELETE /api/projects/:id
 // @access Private
-const deleteProject = (req: Request, res: Response) => {
+const deleteProject = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Delete Project ${req.params.id}` });
-};
+});
 
 module.exports = {
   getProjects,
